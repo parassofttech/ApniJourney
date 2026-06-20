78a82192ef36b524fe61a79f15c55ff3b4e38c40
Paras Sahu <paras2115s@gmail.com>
2026-06-20 12:46:55 +0530

    h1 tag

--- src/Home/HeroSection.jsx
+++ src/Home/HeroSection.jsx
@@ -66,7 +66,7 @@ const HeroSection = () => {
         </motion.div>
 
         {/* Headline with Typewriter-ish Animation */}
-        <motion.h2
+        <motion.h1
           initial={{ opacity: 0, y: 30 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
@@ -76,7 +76,7 @@ const HeroSection = () => {
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 via-blue-400 to-emerald-400 animate-gradient">
             Spirit of India
           </span>
-        </motion.h2>
+        </motion.h1>
 
         <motion.p
           initial={{ opacity: 0 }}
